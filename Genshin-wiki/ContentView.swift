41761717311a2f4a448a16fd6376439277e8e8ce
RixinLi <rli140@ucsc.edu>
2022-05-17 22:54:50 -0700

    Initial Commit

---
+++ Genshin-wiki/ContentView.swift
@@ -0,0 +1,16 @@
+//
+//  ContentView.swift
+//  Genshin-wiki
+//
+//  Created by Rixin Li on 5/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
---
+++ Genshin-wiki/GenshinWiki.swift
@@ -0,0 +1,17 @@
+//
+//  Genshin_wikiApp.swift
+//  Genshin-wiki
+//
+//  Created by Rixin Li on 5/17/22.
+//
+
+import SwiftUI
+
+@main
+struct GenshinWiki: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
